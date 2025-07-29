d243fb03874447b6e86f2d21bebbea32b62b1b57
Maceo <ssjmaceo@gmail.com>
2025-07-28 20:47:19 -0400

    third commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//
+//
+//
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_0acvkum",
+      "template_ref0d6d",
+      event.target,
+      "T2E3orv5NDS7LjDya"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on ssjmaceo@gmail.com"
+      );
+    });
+}
