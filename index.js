81ed1f7a11f09daf1086d220e1bf40244091dfb9
Maceo <ssjmaceo@gmail.com>
2025-07-29 17:28:09 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//
-//
-//
-
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -25,4 +21,38 @@ function contact(event) {
         "The email service is temporarily unavailable. Please contact me directly on ssjmaceo@gmail.com"
       );
     });
+}
+
+let isModalOpen = false
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove ('modal__open')
+    }
+    isModalOpen = true
+    document.body.classList += ' modal__open'
+}
+
+let contrastToggle = false
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += ' dark-mode'
+    }
+    else {
+        document.body.classList.remove('dark-mode')
+    }
+}
+
+const scaleFactor = 1 / 20
+function moveBackground(event) {
+    const shapes = document.querySelectorAll('.shape')
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !==0
+        const bollInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * bollInt}px, ${y * bollInt}px)`
+    }
 }
